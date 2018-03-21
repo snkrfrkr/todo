71691581e2a85fe318d9ab301a06cf57e1d534ad
snkrfrkr <carlos.perry.rbg@gmail.com>
2018-03-21 21:32:16 +0100

    general upadate

---
+++ todo-master/app/controllers/items_controller.rb
@@ -0,0 +1,55 @@
+class ItemsController < ApplicationController
+
+    before_action :find_item, only: [:show, :edit, :update, :destroy, :complete]
+
+    def index
+        @items = Item.all.order("created_at DESC")
+    end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+            if @item.save
+                redirect_to root_path
+            else
+                render 'new'
+            end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @item.update(item_params)
+            redirect_to item_path(@item)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @item.destroy
+        redirect_to root_path
+    end
+
+    def complete
+        @item.update_attribute(:completed_at, Time.now)
+          redirect_to root_path
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:title, :description)
+    end
+
+    def find_item
+        @item = Item.find(params[:id])
+    end
+end
